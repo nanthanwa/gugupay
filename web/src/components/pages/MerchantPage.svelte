0d525c08b8c369cc12d057a952c83fd8aa1243c1
ntsd <jo06942@gmail.com>
2024-11-14 12:07:02 +0700

    feat: add merchant cards

--- web/src/components/pages/MerchantPage.svelte
+++ web/src/components/pages/MerchantPage.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import MerchantModal from "@components/common/MerchantModal.svelte";
+  import { suiToString } from "@utils/sui";
 
   let merchantModal: MerchantModal;
 </script>
@@ -9,14 +10,32 @@
     <div class="text-lg">Merchants</div>
     <button
       class="btn btn-primary"
-      on:click={() => {
+      onclick={() => {
         merchantModal.open();
       }}>New Merchant</button
     >
   </div>
-  <div class="bg-base-300 w-full">
+  <div class="w-full">
     <div class="flex flex-col gap-2">
-      <div></div>
+      <div
+        class="card bg-base-100 flex cursor-pointer flex-col items-start justify-start gap-8 rounded-2xl p-4 shadow-xl lg:flex-row lg:items-center"
+      >
+        <img
+          class="mx-auto h-20 w-20 rounded-full object-cover lg:mx-0"
+          src="/apple-touch-icon.png"
+          alt="Merchant Test"
+          onerror={() => {}}
+        />
+        <div class="mr-0 flex flex-col gap-2 lg:mr-auto">
+          <div class="text-xl">Merchant Test</div>
+          <div class="text-md">Description</div>
+        </div>
+        <div
+          class="ml-auto flex flex-row items-center justify-end gap-2 lg:ml-0"
+        >
+          {suiToString(15500000000)} SUI
+        </div>
+      </div>
     </div>
   </div>
 </div>
